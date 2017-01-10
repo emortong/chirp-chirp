5ddb36be27e8b5188a76e9172cd9938ef61f1fba
Estefania Morton <estefania.morton@gmail.com>
2017-01-09 14:45:56 -1000

    working

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,14 @@
 function chirp(n){
-  // FIX ME
+  if(n === 1) {
+    return 'chirp';
+  }
+
+  return 'chirp ' + chirp(--n)
+
 }
 
-$(document).ready(function(){
-  $("#result").html(chirp(3));
-});
+// $(document).ready(function(){
+//   $("#result").html(chirp(3));
+// });
+
+console.log(chirp(3))
